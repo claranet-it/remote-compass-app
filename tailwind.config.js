a8d7ef5b95585a2844646ee5915105b0d83fbacb
Gianluca La Manna <gl.lamanna@gmail.com>
2023-10-17 12:33:21 +0200

    move custom colors to extend section

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,14 +2,15 @@
 export default {
   content: ['./src/**/*.{html,js,ts,tsx}'],
   theme: {
-    colors: {
-      transparent: 'transparent',
-      'primary': {
-        light: '#e00000',
-        dark: '#e11b22',
+    extend: {
+      colors: {
+        transparent: 'transparent',
+        'primary': {
+          light: '#e00000',
+          dark: '#e11b22',
+        },
       },
     },
-    extend: {},
   },
   plugins: [],
 }
