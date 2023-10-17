6e5d4478aa61d1b2e23681f15a4d1f00e4b916e3
Gianluca La Manna <gl.lamanna@gmail.com>
2023-10-17 12:06:07 +0200

    add tailwind config

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./src/**/*.{html,js,ts,tsx}'],
+  theme: {
+    colors: {
+      transparent: 'transparent',
+      'primary': {
+        light: '#e00000',
+        dark: '#e11b22',
+      },
+    },
+    extend: {},
+  },
+  plugins: [],
+}
+
