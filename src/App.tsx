32a812da92b8a19c092adb13bca78d390511896a
Gianluca La Manna <gl.lamanna@gmail.com>
2023-10-18 12:34:15 +0200

    WIP: get svg after submit form

--- src/App.tsx
+++ src/App.tsx
@@ -1,54 +1,35 @@
-// import { useForm, SubmitHandler } from "react-hook-form"
-
-import { FormEvent } from "react";
-
-
-// type Inputs = {
-//   example: string
-//   exampleRequired: string
-// }
-
+import React from "react";
+import { FormEvent, useEffect, useState } from "react";
+import Confetti from "./components/Confetti";
 
 export default function App() {
-  // const {
-  //   register,
-  //   handleSubmit,
-  //   formState: { errors },
-  // } = useForm<Inputs>()
-  // const onSubmit: SubmitHandler<Inputs> = (data) => console.log(data)
+
+const [element, setElement] = useState(false)
+const [confetti, setConfetti] = useState(false)
 
   const handleSubmit = (event: FormEvent<HTMLFormElement>) => {
     event.preventDefault();
     console.log('Click submit form')
+
+    setElement(true)
+    
   };
 
-  // const createURL = (userId: string, withDate = true) : string => {
-  //   const base = `${window.location.origin}/api/${userId}.svg`;
-  //   return withDate ? `${base}?date=${Date.now()}}` : base;
-  // };
+  useEffect(() => {
+    if (element) {
+      setTimeout(() => {
+        window.scrollTo({
+          top: document.body.scrollHeight,
+          behavior: "smooth"
+        });
+      }, 0); 
+      setTimeout(() => {
+        setConfetti(true)
+      }, 1000)
+    }
+  }, [element]);
 
   return (
-    /* "handleSubmit" will validate your inputs before invoking "onSubmit" */
-
-    // <div className='bg-primary-dark'>
-    //   <button className="bg-primary-dark">TEST DI PROVA</button>
-
-    // <form onSubmit={handleSubmit(onSubmit)}>
-    //   {/* register your input into the hook by invoking the "register" function */}
-    //   <input defaultValue="test" {...register("example")} />
-
-
-    //   {/* include validation with required or other standard HTML validation rules */}
-    //   <input {...register("exampleRequired", { required: true })} />
-    //   {/* errors will return when field validation fails  */}
-    //   {errors.exampleRequired && <span>This field is required</span>}
-
-
-    //   <input type="submit" />
-    // </form>
-    // </div>
-
-
     <div className="min-h-screen flex items-center">
       <div className="w-full">
 
@@ -64,15 +45,62 @@ export default function App() {
               />
             </div>
 
+            <label htmlFor="PRESENCE IN THE OFFICE" className="block mb-2 font-bold text-gray-600">Presence in the office</label>
             <div className="flex items-center mb-4">
-              <input type="radio" id="opzione1" name="opzione" className="inline-block h-5 w-5 border border-gray-300 accent-primary-light rounded-full transition duration-300 ease-in-out transform hover:scale-125 cursor-pointer" />
+              <input type="radio" id="opzione1" name="opzione" className="inline-block ml-2 h-5 w-5 border border-gray-300 accent-primary-light rounded-full transition duration-300 ease-in-out transform hover:scale-125 cursor-pointer" />
               <label htmlFor="opzione1" className="ml-2 text-gray-700">Opzione 1</label>
+
+              <input type="radio" id="opzione2" name="opzione" className="inline-block ml-2 h-5 w-5 border border-gray-300 accent-primary-light rounded-full transition duration-300 ease-in-out transform hover:scale-125 cursor-pointer" />
+              <label htmlFor="opzione2" className="ml-2 text-gray-700">Opzione 2</label>
             </div>
 
+            <label htmlFor="COMPENSATION" className="block mb-2 font-bold text-gray-600">Compensation</label>
+            <div className="flex items-center mb-4">
+
+            <input type="radio" id="opzione1" name="opzione" className="inline-block ml-2 h-5 w-5 border border-gray-300 accent-primary-light rounded-full transition duration-300 ease-in-out transform hover:scale-125 cursor-pointer" />
+              <label htmlFor="opzione1" className="ml-2 text-gray-700">Opzione 1</label>
+
+              <input type="radio" id="opzione2" name="opzione" className="inline-block ml-2 h-5 w-5 border border-gray-300 accent-primary-light rounded-full transition duration-300 ease-in-out transform hover:scale-125 cursor-pointer" />
+              <label htmlFor="opzione2" className="ml-2 text-gray-700">Opzione 2</label>
+            </div>
+
+            <label htmlFor="MEETINGS" className="block mb-2 font-bold text-gray-600">Meetings</label>
+            <div className="flex items-center mb-4">
+            <input type="radio" id="opzione1" name="opzione" className="inline-block ml-2 h-5 w-5 border border-gray-300 accent-primary-light rounded-full transition duration-300 ease-in-out transform hover:scale-125 cursor-pointer" />
+              <label htmlFor="opzione1" className="ml-2 text-gray-700">Opzione 1</label>
+
+              <input type="radio" id="opzione2" name="opzione" className="inline-block ml-2 h-5 w-5 border border-gray-300 accent-primary-light rounded-full transition duration-300 ease-in-out transform hover:scale-125 cursor-pointer" />
+              <label htmlFor="opzione2" className="ml-2 text-gray-700">Opzione 2</label>
+            </div>
+
+            <label htmlFor="COMUNICATION" className="block mb-2 font-bold text-gray-600">Communication</label>
+            <div className="flex items-center mb-4">
+            <input type="radio" id="opzione1" name="opzione" className="inline-block ml-2 h-5 w-5 border border-gray-300 accent-primary-light rounded-full transition duration-300 ease-in-out transform hover:scale-125 cursor-pointer" />
+              <label htmlFor="opzione1" className="ml-2 text-gray-700">Opzione 1</label>
+
+              <input type="radio" id="opzione2" name="opzione" className="inline-block ml-2 h-5 w-5 border border-gray-300 accent-primary-light rounded-full transition duration-300 ease-in-out transform hover:scale-125 cursor-pointer" />
+              <label htmlFor="opzione2" className="ml-2 text-gray-700">Opzione 2</label>
+            </div>
+
+            <label htmlFor="GOVERNANCE" className="block mb-2 font-bold text-gray-600">Governance</label>
+            <div className="flex items-center mb-4">
+            <input type="radio" id="opzione1" name="opzione" className="inline-block ml-2 h-5 w-5 border border-gray-300 accent-primary-light rounded-full transition duration-300 ease-in-out transform hover:scale-125 cursor-pointer" />
+              <label htmlFor="opzione1" className="ml-2 text-gray-700">Opzione 1</label>
+
+              <input type="radio" id="opzione2" name="opzione" className="inline-block ml-2 h-5 w-5 border border-gray-300 accent-primary-light rounded-full transition duration-300 ease-in-out transform hover:scale-125 cursor-pointer" />
+              <label htmlFor="opzione2" className="ml-2 text-gray-700">Opzione 2</label>
+            </div>
 
             <button className="bg-primary-light active:scale-95 opacity-80 block w-full text-white font-bold p-4 rounded-lg transition duration-300 ease-in-out hover:opacity-100">Ottieni SVG</button>
           </form>
         </div>
+
+        {element && (
+          <>
+        <div style={{ height: "800px"}}></div>
+        {confetti && <Confetti/>}
+        </>
+        )} 
       </div>
     </div>
   )
---
+++ src/components/Confetti.tsx
@@ -0,0 +1,73 @@
+/* eslint-disable react-hooks/exhaustive-deps */
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import React from 'react';
+import { useCallback, useEffect, useRef } from 'react';
+
+import ReactCanvasConfetti from 'react-canvas-confetti';
+
+type ConfettiRefType = {
+    (options: any): void; // Adjust the type of options as per your confetti library documentation
+  };
+  
+export default function Confetti() {
+    
+    const refAnimationInstance = useRef<ConfettiRefType | null>(null); // Specify the type of the ref
+
+  const getInstance = useCallback(instance => {
+    refAnimationInstance.current = instance;
+  }, []);
+
+  const makeShot = useCallback((particleRatio, opts) => {
+    refAnimationInstance.current &&
+      refAnimationInstance.current({
+        ...opts,
+        origin: { y: 0.7 },
+        particleCount: Math.floor(200 * particleRatio)
+      });
+  }, []);
+
+  useEffect(() => fire(), []);
+
+  const fire = useCallback(() => {
+    makeShot(0.25, {
+      spread: 26,
+      startVelocity: 55
+    });
+
+    makeShot(0.2, {
+      spread: 60
+    });
+
+    makeShot(0.35, {
+      spread: 100,
+      decay: 0.91,
+      scalar: 0.8
+    });
+
+    makeShot(0.1, {
+      spread: 120,
+      startVelocity: 25,
+      decay: 0.92,
+      scalar: 1.2
+    });
+
+    makeShot(0.1, {
+      spread: 120,
+      startVelocity: 45
+    });
+  }, [makeShot]);
+
+  return (
+    <ReactCanvasConfetti
+      refConfetti={getInstance}
+      style={{
+        position: 'fixed',
+        pointerEvents: 'none',
+        width: '100%',
+        height: '100%',
+        top: 0,
+        left: 0
+      }}
+    />
+  );
+}
