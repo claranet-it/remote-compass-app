6a3f09a7915757f352c00ced543244a1380bc316
Gianluca La Manna <gl.lamanna@gmail.com>
2023-10-19 12:50:39 +0200

    mock: radio button and errors + processing button

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,53 @@
-import React from "react";
-import { FormEvent, useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import Confetti from "./components/Confetti";
+import { SubmitHandler, useForm } from "react-hook-form";
+import Spinner from "./assets/spinner";
+
+
+type Inputs = {
+  title: string
+  exampleRequired: string
+  presenceOffice: 'in-office' | 'in-office-4-5' | 'office-first' | 'remote-first' | 'remote-only'
+}
+
 
 export default function App() {
 
-const [element, setElement] = useState(false)
-const [confetti, setConfetti] = useState(false)
+  const [element, setElement] = useState(false)
+  const [confetti, setConfetti] = useState(false)
+  const [isLoading, setIsLoading] = useState(false)
 
-  const handleSubmit = (event: FormEvent<HTMLFormElement>) => {
-    event.preventDefault();
-    console.log('Click submit form')
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<Inputs>()
+  const onSubmit: SubmitHandler<Inputs> = () => {
 
-    setElement(true)
-    
-  };
+       //MOCK
+       setIsLoading(true)
+
+       setTimeout(() => {
+        setIsLoading(false)
+        setElement(true)
+        }, 3000)
+        //MOCK
+  }
 
   useEffect(() => {
     if (element) {
+      //MOCK
       setTimeout(() => {
         window.scrollTo({
           top: document.body.scrollHeight,
           behavior: "smooth"
         });
-      }, 0); 
+      }, 0);
       setTimeout(() => {
         setConfetti(true)
       }, 1000)
+      //MOCK
+      
     }
   }, [element]);
 
@@ -33,74 +55,62 @@ const [confetti, setConfetti] = useState(false)
     <div className="min-h-screen flex items-center">
       <div className="w-full">
 
-        <div className="bg-white p-10 rounded-lg shadow-xl md:w-3/4 mx-auto lg:w-1/2">
-          <form onSubmit={handleSubmit}>
+        <div className="bg-white p-10 rounded-lg shadow-xl md:auto mx-auto lg:w-1/2">
+          <form onSubmit={handleSubmit(onSubmit)}>
             <div className="mb-5">
               <label htmlFor="title" className="block mb-2 font-bold text-gray-600">Title</label>
-              <input
-                type="text"
-                name="title"
+              <input {...register("title", { required: true })}
                 placeholder="Widget title"
                 className="border border-gray-300 shadow p-3 w-full rounded"
               />
+              {errors.title && <span className="text-primary-light">Questo campo e' obbligatorio</span>}
             </div>
 
-            <label htmlFor="PRESENCE IN THE OFFICE" className="block mb-2 font-bold text-gray-600">Presence in the office</label>
-            <div className="flex items-center mb-4">
-              <input type="radio" id="opzione1" name="opzione" className="inline-block ml-2 h-5 w-5 border border-gray-300 accent-primary-light rounded-full transition duration-300 ease-in-out transform hover:scale-125 cursor-pointer" />
-              <label htmlFor="opzione1" className="ml-2 text-gray-700">Opzione 1</label>
+            <label htmlFor="PRESENCE IN THE OFFICE" className="block mb-2 font-bold text-gray-600"> Presence in the office </label>
 
-              <input type="radio" id="opzione2" name="opzione" className="inline-block ml-2 h-5 w-5 border border-gray-300 accent-primary-light rounded-full transition duration-300 ease-in-out transform hover:scale-125 cursor-pointer" />
-              <label htmlFor="opzione2" className="ml-2 text-gray-700">Opzione 2</label>
+            <div className="mb-4">
+              <div className="flex flex-col sm:flex-row sm:items-start  lg:space-x-6 ">
+
+                <input type="radio"
+                  {...register('presenceOffice', { required: true })}
+                  className="inline-block ml-2 h-5 w-5 border border-gray-300 accent-primary-light rounded-full transition duration-300 ease-in-out transform hover:scale-125 cursor-pointer" />
+                <label className="ml-2 text-gray-700">In-office 5/5</label>
+
+                <input type="radio" 
+                {...register('presenceOffice', { required: true })} 
+                className="inline-block ml-2 h-5 w-5 border border-gray-300 accent-primary-light rounded-full transition duration-300 ease-in-out transform hover:scale-125 cursor-pointer" />
+                <label className="ml-2 text-gray-700">In-office 1...4/5</label>
+
+                <input type="radio" 
+                {...register('presenceOffice', { required: true })} 
+                className="inline-block ml-2 h-5 w-5 border border-gray-300 accent-primary-light rounded-full transition duration-300 ease-in-out transform hover:scale-125 cursor-pointer" />
+                <label className="ml-2 text-gray-700">Office-first Hybrid</label>
+
+                <input type="radio" 
+                {...register('presenceOffice', { required: true })} 
+                className="inline-block ml-2 h-5 w-5 border border-gray-300 accent-primary-light rounded-full transition duration-300 ease-in-out transform hover:scale-125 cursor-pointer" />
+                <label className="ml-2 text-gray-700">Remote-first Hybrid</label>
+
+                <input type="radio" 
+                {...register('presenceOffice', { required: true })} 
+                className="inline-block ml-2 h-5 w-5 border border-gray-300 accent-primary-light rounded-full transition duration-300 ease-in-out transform hover:scale-125 cursor-pointer" />
+                <label className="ml-2 text-gray-700">Remote-only</label>
+              </div>
+              {errors.presenceOffice && <span className="text-primary-light">Questo campo e' obbligatorio</span>}
             </div>
 
-            <label htmlFor="COMPENSATION" className="block mb-2 font-bold text-gray-600">Compensation</label>
-            <div className="flex items-center mb-4">
-
-            <input type="radio" id="opzione1" name="opzione" className="inline-block ml-2 h-5 w-5 border border-gray-300 accent-primary-light rounded-full transition duration-300 ease-in-out transform hover:scale-125 cursor-pointer" />
-              <label htmlFor="opzione1" className="ml-2 text-gray-700">Opzione 1</label>
-
-              <input type="radio" id="opzione2" name="opzione" className="inline-block ml-2 h-5 w-5 border border-gray-300 accent-primary-light rounded-full transition duration-300 ease-in-out transform hover:scale-125 cursor-pointer" />
-              <label htmlFor="opzione2" className="ml-2 text-gray-700">Opzione 2</label>
-            </div>
-
-            <label htmlFor="MEETINGS" className="block mb-2 font-bold text-gray-600">Meetings</label>
-            <div className="flex items-center mb-4">
-            <input type="radio" id="opzione1" name="opzione" className="inline-block ml-2 h-5 w-5 border border-gray-300 accent-primary-light rounded-full transition duration-300 ease-in-out transform hover:scale-125 cursor-pointer" />
-              <label htmlFor="opzione1" className="ml-2 text-gray-700">Opzione 1</label>
-
-              <input type="radio" id="opzione2" name="opzione" className="inline-block ml-2 h-5 w-5 border border-gray-300 accent-primary-light rounded-full transition duration-300 ease-in-out transform hover:scale-125 cursor-pointer" />
-              <label htmlFor="opzione2" className="ml-2 text-gray-700">Opzione 2</label>
-            </div>
-
-            <label htmlFor="COMUNICATION" className="block mb-2 font-bold text-gray-600">Communication</label>
-            <div className="flex items-center mb-4">
-            <input type="radio" id="opzione1" name="opzione" className="inline-block ml-2 h-5 w-5 border border-gray-300 accent-primary-light rounded-full transition duration-300 ease-in-out transform hover:scale-125 cursor-pointer" />
-              <label htmlFor="opzione1" className="ml-2 text-gray-700">Opzione 1</label>
-
-              <input type="radio" id="opzione2" name="opzione" className="inline-block ml-2 h-5 w-5 border border-gray-300 accent-primary-light rounded-full transition duration-300 ease-in-out transform hover:scale-125 cursor-pointer" />
-              <label htmlFor="opzione2" className="ml-2 text-gray-700">Opzione 2</label>
-            </div>
-
-            <label htmlFor="GOVERNANCE" className="block mb-2 font-bold text-gray-600">Governance</label>
-            <div className="flex items-center mb-4">
-            <input type="radio" id="opzione1" name="opzione" className="inline-block ml-2 h-5 w-5 border border-gray-300 accent-primary-light rounded-full transition duration-300 ease-in-out transform hover:scale-125 cursor-pointer" />
-              <label htmlFor="opzione1" className="ml-2 text-gray-700">Opzione 1</label>
-
-              <input type="radio" id="opzione2" name="opzione" className="inline-block ml-2 h-5 w-5 border border-gray-300 accent-primary-light rounded-full transition duration-300 ease-in-out transform hover:scale-125 cursor-pointer" />
-              <label htmlFor="opzione2" className="ml-2 text-gray-700">Opzione 2</label>
-            </div>
-
-            <button className="bg-primary-light active:scale-95 opacity-80 block w-full text-white font-bold p-4 rounded-lg transition duration-300 ease-in-out hover:opacity-100">Ottieni SVG</button>
+            <button className="inline-flex items-center justify-center font-semibold leading-6  shadow rounded-lg active:scale-95 opacity-80 w-full text-white bg-primary-light p-4 transition duration-300 ease-in-out hover:opacity-100 cursor-pointer">
+            {isLoading ? (<><Spinner/>  <span>Processing...</span></>) : <span>Ottieni svg</span>} 
+            </button>
           </form>
         </div>
 
         {element && (
           <>
-        <div style={{ height: "800px"}}></div>
-        {confetti && <Confetti/>}
-        </>
-        )} 
+            <div style={{ height: "800px" }}></div>
+            {confetti && <Confetti />}
+          </>
+        )}
       </div>
     </div>
   )
