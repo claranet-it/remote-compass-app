6b323cade8d0ac6fc754b9572d4268d758bbeeba
Gianluca La Manna <gl.lamanna@gmail.com>
2023-10-20 14:50:55 +0200

    add back button

--- src/components/SvgContainer.tsx
+++ src/components/SvgContainer.tsx
@@ -1,5 +1,8 @@
 //import Copy from "../assets/copy"
 
+import Back from "../assets/back"
+import Copy from "../assets/copy"
+
 type PropsSvgContainer = {
     data: string | undefined
 }
@@ -11,27 +14,28 @@ export const SvgContainer: React.FC<PropsSvgContainer> = ({
 
     return (
 
-        
-<div className="p-20 w-full md:w-1/2">
-  <div className="bg-white rounded-lg shadow-lg">
-    <img src={data} alt="" className="rounded-t-lg"/>
-    <div className="p-6">
-      <h2 className="font-bold mb-2 text-2xl text-purple-800">Card with an image
-      </h2>
-      <p className="text-purple-700 mb-2">This is a little bit better of a card!</p>
-      <a href="#" className="text-purple-600 hover:text-purple-500 underline text-sm">Read More 👉</a>
-    </div>
+        <div className="p-20 w-full">
+            <div className="bg-white rounded-lg shadow-lg">
+                <img src={data} alt="svg" width="100%" height="100%" className="rounded-t-lg" />
+                <div className="p-6">
+                    <h2 className="font-bold mb-2 text-2xl text-dark-light">Take your image</h2>
+                    <div className="flex justify-end space-x-3">
+                    <button title="Back to form" className="inline-flex items-center justify-center  shadow rounded-lg active:scale-95 opacity-80  text-secondary-light bg-tertiary-light p-4 transition duration-300 ease-in-out hover:opacity-100 cursor-pointer ">
+                        <span className="font-bold mr-2">Back</span> <Back/>
+                    </button>
+                    <button title="Copy on clipboard" className="inline-flex items-center justify-center  shadow rounded-lg active:scale-95 opacity-80  text-white bg-primary-light p-4 transition duration-300 ease-in-out hover:opacity-100 cursor-pointer">
+                    <Copy/>
+                </button>            
+                </div>    
+                </div>
+            </div>
+        </div>
 
-  </div>
-</div>
-      
     )
-
-
 }
 
 
-  {/* <div className={`flex items-center mt-4 p-10`} >
+{/* <div className={`flex items-center mt-4 p-10`} >
             <img src={data} width="100%" height="100%" />
             <div className="flex m-4 items-center justify-center space-x-4 w-100">
                 <input disabled
