c63abb0cff85749c6c8793039fcfd8b32b1c3976
Gianluca La Manna <gl.lamanna@gmail.com>
2023-10-31 12:02:04 +0100

    chore: reintroduce mandatory fields

--- src/components/form/RadioFieldset.tsx
+++ src/components/form/RadioFieldset.tsx
@@ -20,7 +20,7 @@ export const RadioFieldset: React.FC<PropsRadio> = ({
     return values.map((value, index) => (
         <label className="flex p-2" key={index}>
             <input type="radio"
-                {...register(nameRegister, { required: false })}
+                {...register(nameRegister, { required: true })}
                 value={value}
                 name={nameRegister as string}
                 className="inline-block ml-2 h-5 w-5 border border-gray-300 accent-primary-light rounded-full transition duration-300 ease-in-out transform hover:scale-125 cursor-pointer" />
